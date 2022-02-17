c3a2eb63795ccdf3820a72c220d1957c503c5c8e
guohaizhen <guohaizhen@richinfo.cn>
2022-02-17 15:42:02 +0800

    修改2

--- src/main/java/com/HelloWorld.java
+++ src/main/java/com/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("修改1");
+        System.out.println("修改2");
     }
 
 }
